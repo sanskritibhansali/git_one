b55e2f20d87eafa318eb8f348795ec72109f363f
sanskritibhansali <sanskritibhansali@gmail.com>
2018-06-13 23:31:43 +0530

    mag_meter

---
+++ mag_meter.py
@@ -0,0 +1,11 @@
+import numpy as np
+v_x_mag=input()  #taking input from igrf x-component
+v_y_mag=input()                          #y-component
+v_z_mag=input()                          #z-component
+mag_bias=0.0003   #bias taken from data sheet of magnetometer used in pratham
+def v_mag(v_x_mag,v_y_mag,v_z_mag,mag_bias):    
+          x=v_x_mag+np.random.normal()+mag_bias  #adding random error and bias in each component.
+          y=v_y_mag+np.random.normal()+mag_bias
+          z=v_z_mag+np.random.normal()+mag_bias
+          v_mag_m=np.array([x,y,z])
+
