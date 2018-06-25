0d51f15370baefa4c1124e98ebf5e017d9d6c77b
sanskritibhansali <37136771+sanskritibhansali@users.noreply.github.com>
2018-06-25 10:21:41 +0530

    Add files via upload

--- constants.py
+++ constants.py
@@ -10,7 +10,9 @@ v_S3=np.array([0,1,0])
 v_S4=np.array([0,-1,0])
 v_S5=np.array([0,0,1])
 v_S6=np.array([0,0,-1])
-
+#maximum voltage reading observed= 3.3
+#minimum voltage reading observed= 0
+#minimum voltage at which sensor is taken to be in light regiom is 1.894
 
 #magnetometer
 mag_bias=0.0003
