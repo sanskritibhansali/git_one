0cb699e93b51caea939901f0ee17461701a65030
sanskritibhansali <37136771+sanskritibhansali@users.noreply.github.com>
2018-06-25 10:06:25 +0530

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,16 @@
+#constants used in sensor modelling
+import numpy as np
+#sunsensor
+SS_GAIN=3.3 #gain factor as the readings from sunsensor are in voltage.
+SS_QUANTIZER=pow(2,10)-1  #because we have 10 bit adc
+MAX_ANGLE=55            #taken from pratham's simulink model,can be adjusted, it is the 1/2 of threshold value of angle which will be taken in FOV
+v_S1=np.array([1,0,0])   #vectors perpendicular to sensors.
+v_S2=np.array([-1,0,0])
+v_S3=np.array([0,1,0])
+v_S4=np.array([0,-1,0])
+v_S5=np.array([0,0,1])
+v_S6=np.array([0,0,-1])
+
+
+#magnetometer
+mag_bias=0.0003
