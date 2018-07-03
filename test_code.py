c9bc3c7da6ed5e813120d733f21846f7d46191a3
sanskritibhansali <37136771+sanskritibhansali@users.noreply.github.com>
2018-07-03 12:29:47 +0530

    Add files via upload

--- test_code.py
+++ test_code.py
@@ -1,5 +1,5 @@
 import ddt
-import sanskriti
+import sun_sensor_modelling
 import unittest	#testing library
 import numpy as np
 from ddt import ddt,file_data,unpack,data
@@ -9,9 +9,15 @@ class TestLatLon(unittest.TestCase):
 	@unpack
 	def test_cases_file(self,value):
 	    given1,given2,given3,given4,given5,given6,given7,given8,given9,expected = np.asarray(value[0]),np.asarray(value[1]),np.asarray(value[2]),np.asarray(value[3]),np.asarray(value[4]),np.asarray(value[5]),np.asarray(value[6]),value[7],value[8],value[9]	#asarray to convert list to array
-	    result = sanskriti.ad_convertor(given1,given2,given3,given4,given5,given6,given7,given8,given9)
+	    result = sun_sensor_modelling.ad_convertor(given1,given2,given3,given4,given5,given6,given7,given8,given9)
 	    self.assertTrue(np.allclose([result[0],result[1],result[2],result[3],result[4],result[5]],expected))
-	   	  	   	    
+	
+	@file_data("test_light.json")
+	@unpack
+	def test_light(self,value):
+	    given1,given2,expected=np.asarray(value[0]),value[1],np.asarray(value[2])
+	    result=sun_sensor_modelling.light(given1,given2)
+	    self.assertTrue(np.allclose([result[0],result[1],result[2],result[3],result[4],result[5]],expected))   	  	   	       	  	   	    
 
 if __name__=='__main__':
 	unittest.main(verbosity=2)	   	   	   	   
