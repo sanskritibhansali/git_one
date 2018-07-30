10a3441011497946e24fc0c32ec30b038ec42ea8
sanskritibhansali <37136771+sanskritibhansali@users.noreply.github.com>
2018-07-30 23:57:16 +0530

    Update test_code.py

--- test_code.py
+++ test_code.py
@@ -9,7 +9,7 @@ class TestLatLon(unittest.TestCase):
 	@unpack
 	def test_ad_convertor(self,value):
 	    given1,given2,given3,given4,given5,given6,given7,given8,given9,expected = np.asarray(value[0]),np.asarray(value[1]),np.asarray(value[2]),np.asarray(value[3]),np.asarray(value[4]),np.asarray(value[5]),np.asarray(value[6]),value[7],value[8],value[9]	#asarray to convert list to array
-	    result = sun_sensor_modelling.ad_convertor(given1,given2,given3,given4,given5,given6,given7,given8,given9)
+	    result = sun_sensor_modelling.ad_Convertor(given1,given2,given3,given4,given5,given6,given7,given8,given9)
 	    self.assertTrue(np.allclose([result[0],result[1],result[2],result[3],result[4],result[5]],expected))
 	
 	@file_data("test_light.json")
@@ -24,7 +24,7 @@ class TestLatLon(unittest.TestCase):
 	@unpack
 	def test_sv_calc(self,value):
 	    given1,given2,given3,expected = np.asarray(value[0]),value[1],value[2],np.asarray(value[3])	#asarray to convert list to array
-	    result = sun_sensor_modelling.calc_sv(given1,given2,given3)
+	    result = sun_sensor_modelling.calc_SV(given1,given2,given3)
 	    self.assertTrue(np.allclose([result[0],result[1],result[2]],expected))
 	   	  	   	       	  	   	    
 
